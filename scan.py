d3f5b177d24d4d6dadcadf36a495a9efd5316814
mutesplash <49622611+mutesplash@users.noreply.github.com>
2022-01-22 03:02:22 -0500

    Scan and activate Lego Mario bluetooth objects

---
+++ scan.py
@@ -0,0 +1,52 @@
+import sys
+import platform
+import asyncio
+import time
+from bleak import BleakScanner, BleakClient
+
+from BTLegoMario import BTLegoMario
+
+mario_devices = {}
+
+async def detect_device_callback(device, advertisement_data):
+	global mario_devices
+
+	if device:
+		mario_device = BTLegoMario.which_device(advertisement_data)
+		if mario_device:
+			if not device.address in mario_devices:
+				mario_devices[device.address] = BTLegoMario()
+				await mario_devices[device.address].connect(device, advertisement_data)
+			else:
+				if not mario_devices[device.address].connected:
+					await mario_devices[device.address].connect(device, advertisement_data)
+				else:
+					print("Refusing to reconnect to "+mario_devices[device.address].which_brother)
+		else:
+			# "LEGO Mario_x_y"
+			if device.name and device.name.startswith("LEGO Mario"):
+				if advertisement_data and advertisement_data.manufacturer_data:
+					print("UNKNOWN LEGO MARIO",mario_device, device.address, "RSSI:", device.rssi, advertisement_data)
+				else:
+					#print("Found some useless Mario broadcast without the manufacturer or service UUIDs")
+					pass
+
+async def callbackscan(duration=10):
+	scanner = BleakScanner()
+	print("Ready to find LEGO Mario!")
+	scanner.register_detection_callback(detect_device_callback)
+	print("Scanning...")
+	await scanner.start()
+	await asyncio.sleep(duration)
+	await scanner.stop()
+
+	#print("Scan results...")
+	#for d in scanner.discovered_devices:
+	#	print(d)
+
+run_seconds = 60
+asyncio.run(callbackscan(run_seconds))
+if len(mario_devices):
+	print("Done with LEGO Mario session after "+str(run_seconds)+" seconds...")
+else:
+	print("Didn't connect to a LEGO Mario.  Quitting.")
