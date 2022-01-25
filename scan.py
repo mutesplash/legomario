2928f433f17e13210035ba1a5208d40e26c5dfbc
mutesplash <49622611+mutesplash@users.noreply.github.com>
2022-01-25 16:37:41 -0500

    Move JSON file loading into here

--- scan.py
+++ scan.py
@@ -6,27 +6,24 @@ from bleak import BleakScanner, BleakClient
 import io
 import os
 from pathlib import Path
+import json
 
 from BTLegoMario import BTLegoMario
 
 mario_devices = {}
+code_data = None
+
 json_code_file = "../mariocodes.json"
+run_seconds = 60
 
 async def detect_device_callback(device, advertisement_data):
 	global mario_devices
-	global json_code_file
-
+	global code_data
 	if device:
 		mario_device = BTLegoMario.which_device(advertisement_data)
 		if mario_device:
 			if not device.address in mario_devices:
-				check_file = Path(os.path.expanduser(json_code_file))
-				if check_file.is_file():
-					with open(check_file, "rb") as f:
-						mario_devices[device.address] = BTLegoMario(f.read())
-				else:
-					print("Known code database (mariocodes.json) NOT loaded!")
-					mario_devices[device.address] = BTLegoMario()
+				mario_devices[device.address] = BTLegoMario(code_data)
 				await mario_devices[device.address].connect(device, advertisement_data)
 			else:
 				if not mario_devices[device.address].connected:
@@ -35,6 +32,7 @@ async def detect_device_callback(device, advertisement_data):
 					print("Refusing to reconnect to "+mario_devices[device.address].which_brother)
 		else:
 			# "LEGO Mario_x_y"
+			# Spike prime hub starts with "LEGO Hub" but you have to pair with that, not BTLE
 			if device.name and device.name.startswith("LEGO Mario"):
 				if advertisement_data and advertisement_data.manufacturer_data:
 					print("UNKNOWN LEGO MARIO",mario_device, device.address, "RSSI:", device.rssi, advertisement_data)
@@ -55,8 +53,22 @@ async def callbackscan(duration=10):
 	#for d in scanner.discovered_devices:
 	#	print(d)
 
-run_seconds = 60
-asyncio.run(callbackscan(run_seconds))
+check_file = Path(os.path.expanduser(json_code_file))
+if check_file.is_file():
+	with open(check_file, "rb") as f:
+		try:
+			code_data = json.loads(f.read())
+		except ValueError as e:  # also JSONDecodeError
+			print("Unable to load code translation JSON:"+str(e))
+
+if not code_data:
+	print("Known code database (mariocodes.json) NOT loaded!")
+
+try:
+	asyncio.run(callbackscan(run_seconds))
+except KeyboardInterrupt:
+	print("Recieved keyboard interrupt, stopping.")
+
 if len(mario_devices):
 	print("Done with LEGO Mario session after "+str(run_seconds)+" seconds...")
 else:
