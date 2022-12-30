0a009cc623a3af04909ee8524ecef33f720f4ae3
mutesplash <49622611+mutesplash@users.noreply.github.com>
2022-12-29 19:33:04 -0500

    Various updates
    New colors that register with the scanner (Alerted by @tomalphin)
    Brute forced the Ice Flower pants code
    Erase Peach's icon (Need to know what Mario and Luigi's no-icon names are...)
    Force the version to be displayed on connect

--- BTLegoMario.py
+++ BTLegoMario.py
@@ -67,7 +67,7 @@ class BTLegoMario(BTLego):
 	# pants
 	#	'pants': (pants_codes)
 	# info
-	#	'brother':		'mario', 'luigi'
+	#	'brother':		'mario', 'luigi', 'peach'
 	#	'icon': 		((app_icon_names),(app_icon_color_names))
 	#	'batt':			(percentage)
 	#	'power': 		'turning_off', 'disconnecting'
@@ -129,58 +129,83 @@ class BTLegoMario(BTLego):
 	code_data = None
 	gr_codespace = {}
 	br_codespace = {}
+	# Color scanner is a bit buggy.  Blue works better in latest firmware, but sometimes the scanner doesn't return color messages
 	solid_colors = {
-		19:'white',
-		21:'red',
-		23:'blue',		# For some reason, this doesn't return even if mario puts himself in water on the screen
-		24:'yellow',
-		26:'black',
-		37:'green',
-		106:'orange',	# listed as brown elsewhere, but the NES code "knows" orange and orange scans as this
-		268:'purple',
-		312:'nougat',
-		322:'cyan'
+		19:'white',		# Official Lego color ID for white is 1 and 19 for Light Brown (probably too close to Medium Nougat)
+		21:'red',		# Bright Red
+		23:'blue',		# Bright Blue
+		24:'yellow',	# Bright Yellow
+		26:'black',		# Black
+		37:'green',		# Bright Green
+		106:'orange',	# Bright Orange (listed as 'brown' elsewhere)
+		119:'lime',		# Bright Yellowish Green
+		221:'pink',		# Bright Purple
+		268:'purple',	# Medium Lilac
+		312:'nougat',	# Medium Nougat
+		322:'cyan',		# Medium Azur
+		324:'lavender'	# Medium Lavender
 	}
 
 	# Read the pins facing you with MSB on the left (Mario's right)
+	# Some pants codes simply do not register.  May be some debouncing or max pin-down circuitry
 	pants_codes = {
 		0x0:'no',			# 000 000	Sometimes mario registers 0x2 as no pants, might be a pin problem?
 		0x1:'vacuum',		# 000 001	Poltergust
 		0x2:'no',			# 000 010	Just cover the case and pretend it's normal, who wants to get weird messages and debug them. When you actually PUSH pin 1 (0x2), you get the update icon
 		0x3:'bee',			# 000 011	Acts strange and won't send messages when these pants are on.  Needs more testing
-							# 000 100
+#		0x4:'?4?',			# 000 100
 		0x5:'luigi',		# 000 101
 		0x6:'frog',			# 000 110
 							# 000 111
-							# 001 000	does nothing and doesn't trigger the update icon, perhaps a hidden trigger?  Might also be poltergust?
+#		0x8:'?8?',			# 001 000	does nothing and doesn't trigger the update icon, perhaps a hidden trigger?  Might also be poltergust?
 		0x9:'vacuum_button',# 001 001	Poltergust. Seems to not matter which one is the toggle!
 		0xa:'tanooki',		# 001 010	Leaf icon
 							# 001 011
 		0xc:'propeller',	# 001 100
-							# 001 101
-							# 001 110
-							# 001 111
-							# 010 000
+#		0xd:'?13?',			# 001 101
+#		0xe:'?14?',			# 001 110
+#		0xf:'?15?',			# 001 111
+#		0x10:'?16?',		# 010 000
 		0x11:'cat',			# 010 001	Bell icon
 		0x12:'fire',		# 010 010
 							# 010 011
 		0x14:'penguin',		# 010 100
 							# 010 101
-							# 010 110
+#		0x16:'22?',			# 010 110
 							# 010 111
-							# 011 000
+		0x18:'dress',		# 011 000
 							# 011 001
-							# 011 010
+#		0x1a:'26?',			# 011 010
 							# 011 011
-							# 011 100
+#		0x1c:'28?',			# 011 100
 							# 011 101
 							# 011 110
 							# 011 111
 		0x20:'mario',		# 100 000	Because who has time to deal with errata... doesn't seem to be the SAME pin problem,  When you actually PUSH pin 5 (0x20), you get the update icon
 		0x21:'mario',		# 100 001	Mario pants.  Sometimes mario registers 0x20 as mario pants, might be a pin problem?
 		0x22:'builder',		# 100 010
-
-		0x2a:'cat'			# 101 010	Peach's cat pants
+		0x23:'ice',			# 100 011	Ice flower icon
+#		0x24:'36?',			# 100 100
+#		0x25:'37?',			# 100 101
+#		0x26:'38?',			# 100 110
+							# 100 111
+#		0x28:'40?',			# 101 000
+							# 101 001
+		0x2a:'cat'			# 101 010	Bell icon. Peach's cat pants
+							# 101 011
+#		0x2c:'44?',			# 101 100
+							# 101 101
+							# 101 110
+							# 110 111
+#		0x30:'48?',			# 110 000
+							# 110 001
+							# 110 010
+							# 110 011
+							# 110 100
+							# 110 101
+							# 110 110
+							# 110 111
+#		0x38:'?56?'			# 111 000
 	}
 
 	# Set in the advertising name
@@ -295,6 +320,7 @@ class BTLegoMario(BTLego):
 					await self.request_name_update()
 					# Definitely not sent on connect
 					await self.request_volume_update()
+					await self.request_version_update()
 
 					while self.client.is_connected:
 						await asyncio.sleep(0.05)
@@ -737,6 +763,11 @@ class BTLegoMario(BTLego):
 			# 0x2 0x40 0x1 0x0
 			# 0x1 0x30 0x0 0x0
 
+			# Abrupt jump
+			#peach event data:0x57 0x38 0x0 0x0
+			#peach event data:0x62 0x38 0x0 0x0
+
+
 			if not decoded_something:
 				BTLegoMario.dp(self.which_brother+" event data:"+" ".join(hex(n) for n in data),2)
 		else:
@@ -751,7 +782,7 @@ class BTLegoMario(BTLego):
 			# print(name.encode("utf-8").hex())
 			if name == "LEGO Peach    ":
 				# Four spaces after the name
-				BTLegoMario.dp("Unable to decode stock lego peach icon")
+				BTLegoMario.dp("Peach has no icon or color set")
 			else:
 				BTLegoMario.dp("Unusual advertising name set:"+name)
 			return
@@ -1029,6 +1060,26 @@ class BTLegoMario(BTLego):
 		await self.client.write_gatt_char(BTLegoMario.characteristic_uuid, set_name_bytes)
 		await asyncio.sleep(0.1)
 
+	async def erase_icon(self):
+		if self.which_brother != 'peach':
+			BTLegoMario.dp("ERROR: Don't know how to erase any brother except peach")
+			return
+
+		set_name_bytes = bytearray([
+			0x00,	# len placeholder
+			0x00,	# padding but maybe stuff in the future (:
+			0x1,	# 'hub_properties'
+			0x1,	# 'Advertising Name'
+			0x1		# 'Set'
+		])
+
+		set_name_bytes = set_name_bytes + "LEGO Peach    ".encode()
+
+		set_name_bytes[0] = len(set_name_bytes)
+
+		await self.client.write_gatt_char(BTLegoMario.characteristic_uuid, set_name_bytes)
+		await asyncio.sleep(0.1)
+
 	async def set_volume(self, volume):
 		if volume > 100 or volume < 0:
 			return
@@ -1106,6 +1157,29 @@ class BTLegoMario(BTLego):
 		])
 		await self.client.write_gatt_char(BTLegoMario.characteristic_uuid, name_update_bytes)
 		await asyncio.sleep(0.1)
+
+	async def request_version_update(self):
+		# Triggers hub_properties message
+		name_update_bytes = bytearray([
+			0x05,	# len
+			0x00,	# padding but maybe stuff in the future (:
+			0x1,	# 'hub_properties'
+			0x3,	# 'Firmware version'
+			0x5		# 'Request Update'
+		])
+		await self.client.write_gatt_char(BTLegoMario.characteristic_uuid, name_update_bytes)
+		await asyncio.sleep(0.1)
+
+		name_update_bytes = bytearray([
+			0x05,	# len
+			0x00,	# padding but maybe stuff in the future (:
+			0x1,	# 'hub_properties'
+			0x4,	# 'Hardware version'
+			0x5		# 'Request Update'
+		])
+		await self.client.write_gatt_char(BTLegoMario.characteristic_uuid, name_update_bytes)
+		await asyncio.sleep(0.1)
+
 
 	def port_inport_format_setup_bytes(port, mode, enable):
 		# original hint from https://github.com/salendron/pyLegoMario
