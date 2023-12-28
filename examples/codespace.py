907c4ade8a27f9501a30033dbdcc6c5004732f4b
mutesplash <49622611+mutesplash@users.noreply.github.com>
2023-12-28 00:36:28 -0500

    Only need one code tool example

--- examples/codespace.py
+++ examples/codespace.py
@@ -1,24 +1,6 @@
-from BTLego import Mario
+from BTLego.MarioScanspace import MarioScanspace
 
-import os
-from pathlib import Path
-import json
-
-json_code_file = "../mariocodes.json"
-code_data = None
-check_file = Path(os.path.expanduser(json_code_file))
-if check_file.is_file():
-	with open(check_file, "rb") as f:
-		try:
-			code_data = json.loads(f.read())
-		except ValueError as e:  # also JSONDecodeError
-			print("Unable to load code translation JSON:"+str(e))
-
-if not code_data:
-	print("Known code database (mariocodes.json) NOT loaded!")
-
-mario = Mario(json_code_dict=code_data)
 print("i\tcode\tmirror\tlabel\t\tscanner hex\tbinary")
 print("-----------------------------------------------------------------")
-
-Mario.print_codespace()
+MarioScanspace.import_codefile()
+MarioScanspace.print_codespace()
--- examples/codetool.py
+++
@@ -1,6 +0,0 @@
-from BTLego.MarioScanspace import MarioScanspace
-
-print("i\tcode\tmirror\tlabel\t\tscanner hex\tbinary")
-print("-----------------------------------------------------------------")
-MarioScanspace.import_codefile()
-MarioScanspace.print_codespace()
