ed737baf9098596ae3dafa14d42b03d3cb778ef3
mutesplash <49622611+mutesplash@users.noreply.github.com>
2026-08-11 00:47:07 -0400

    Interactive Duplo Train scanner tweaks

--- BTLego/LPF_Devices/DT_Scanner.py
+++ BTLego/LPF_Devices/DT_Scanner.py
@@ -22,11 +22,12 @@ class DT_Scanner(LPF_Device):
 
 	def decode_pvs(self, port, data):
 		# Mode 0
+		# Won't scan these stationary, must be moving
 		if len(data) == 2:
 			first_bytes = int.from_bytes(data[0:], byteorder="little")
 
 			if first_bytes == 1:
-				# 35962pb04	: White : Sun Icon : Train speed pattern: 0,40,50,60,69
+				# 35962pb04 : Sun Icon : Train speed pattern: 0,40,50,60,69
 				return ('duplotrain_scantag','tab','white' )
 			elif first_bytes == 21:
 				# 35962pb03 : X in Circle Icon: Train speed pattern 0,-5,-10,-15,-10,-5,0 ... 95,91,87,83,79,75,72,69
@@ -36,9 +37,15 @@ class DT_Scanner(LPF_Device):
 				# 35962pb05 : Music Note Icon: NO EVENTS, Toots the horn
 				return ('duplotrain_scantag','tab','yellow' )
 
+				# Violent Yellow and Bright Light Yellow scan the same
+
 			elif first_bytes == 28:
 				# 35962pb02 : Double Arrow Icon: Speed to zero, reverses direction
 				return ('duplotrain_scantag','tab','green' )
+
+			elif first_bytes == 37:
+				# 35962pb14 : Bright Green: Lightning Bolt Icon:
+				return ('duplotrain_scantag','tab','bright green' )
 
 			elif first_bytes == 119:
 				# 35962pb09 : Tree Icon: Speed to zero, plays sounds
@@ -51,6 +58,9 @@ class DT_Scanner(LPF_Device):
 			elif first_bytes == 321:
 				# 35962pb01 : Water Icon: Difficult to scan. Train speed pattern: 0,-50,50,-50,50,-50,50,-50,50,-50,50,55,60,65,69
 				return ('duplotrain_scantag','tab','blue' )
+
+				# 35962pb11 : Dark Azure : Water with sheen Icon:
+				# Same scan code
 
 			elif first_bytes == 324:
 				# 35962pb08 : Star Icon: NO EVENTS, plays sounds for longer than you would expect
@@ -65,8 +75,4 @@ class DT_Scanner(LPF_Device):
 				return ('duplotrain_scantag','tab','reddish orange' )
 
 			else:
-				print(f"UNKNOWN Tab: #{first_bytes}")
-
-				# 35962pb11 : Dark Azure : Water with sheen Icon:
-				# 35962pb14 : Bright Green: Lightning Bolt Icon:
 				return ('duplotrain_scantag','tag', first_bytes )
